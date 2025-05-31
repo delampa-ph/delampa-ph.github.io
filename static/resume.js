f679551e074c98c145a8309b46ca4a1e64cd5b20
Derek Lampa <lampaderek@gmail.com>
2025-05-31 11:32:09 +0800

    Convert work and education info to templates.
    - Add appendInfoHTML in resume.js for work and education info.
    - Refactor work and education info in resume.html to templates.

--- static/resume.js
+++ static/resume.js
@@ -1,4 +1,6 @@
-const CONTACT_HTML = document.getElementById("topInfo");
+const CONTACT_HTML      = document.getElementById("topInfo");
+const WORK_HTML         = document.getElementById("workInfo");
+const EDUCATION_HTML    = document.getElementById("educationInfo");
 
 function appendTopInfoHTML(info){
     for(const infoVal of info) {
@@ -39,9 +41,120 @@ function appendTopInfoHTML(info){
     }
 }
 
+function appendInfoHTML(info, targetDiv) {
+    let siding = 0;
+    for (const infoVal of info) {
+        siding++;
+
+        const containerDiv = document.createElement("div");
+        let textSide = `${siding % 2 == 1 ? "text-left" : "text-end"}`
+        containerDiv.setAttribute("class", `row gap-1 ${textSide}`);
+        targetDiv.append(containerDiv);
+
+        // Logo Side
+        const logoDiv = document.createElement("div");
+        logoDiv.setAttribute("class", "col-sm-3");
+        const logoLink = document.createElement("a");
+        logoLink.setAttribute("href", infoVal.image.link);
+        logoDiv.append(logoLink);
+        const logoImg = document.createElement("img");
+        logoImg.setAttribute("src", infoVal.image.src);
+        logoImg.setAttribute("style", "width: 18vw;");
+        logoLink.append(logoImg);
+
+        // Description Side
+        const infoDiv = document.createElement("div");
+        infoDiv.setAttribute("class", "col");
+
+        const infoName = document.createElement("span");
+        infoName.setAttribute("class", "h4 fw-bold");
+        infoName.innerHTML = infoVal.name;
+        infoDiv.append(infoName);
+
+        const infoDate = document.createElement("p");
+        infoDate.setAttribute("class", "bi bi-calendar-fill fs-6");
+        infoDate.setAttribute("style", "margin-bottom: 0px;");
+        infoDate.innerText = ` ${infoVal.date}`;
+        infoDiv.append(infoDate);
+        
+        let positionIcon = "bi bi-person-vcard-fill"
+        if (infoVal.positionIcon != undefined)
+            positionIcon = infoVal.positionIcon;
+        const infoPosition = document.createElement("p")
+        infoPosition.setAttribute("class", `${positionIcon} fw-bold`);
+        infoPosition.setAttribute("style", "margin-bottom: 1em;")
+        infoPosition.innerText = ` ${infoVal.position}`;
+        infoDiv.append(infoPosition);
+
+        if (infoVal.description != undefined) {
+            const infoDescHeader = document.createElement("i");
+            infoDescHeader.setAttribute("class", "fw-bold fs-4");
+            infoDescHeader.setAttribute("style", "margin-bottom: 0px;");
+            infoDescHeader.innerText = " Description:";
+            infoDiv.append(infoDescHeader);
+
+            const infoDescValue = document.createElement("p");
+            infoDescValue.innerHTML = infoVal.description;
+            infoDiv.append(infoDescValue);
+        }
+
+        if (infoVal.awards != undefined) {
+            const infoAwardHeader = document.createElement("i");
+            infoAwardHeader.setAttribute("class", "fw-bold fs-4");
+            infoAwardHeader.innerText = "Awards:"
+            infoDiv.append(infoAwardHeader);
+
+            const infoAwardContainer = document.createElement("div");
+            infoAwardContainer.setAttribute("class", "container");
+            infoDiv.append(infoAwardContainer);
+
+            const infoAwardCRow = document.createElement("div");
+            let contentAlign = siding % 2 == 1 ? "justify-content-start" : "justify-content-end";
+            infoAwardCRow.setAttribute("class", `row gap-1 row-cols-auto ${contentAlign}`);
+            infoAwardContainer.append(infoAwardCRow);
+
+            const infoAwardTitle = document.createElement("div");
+            infoAwardTitle.setAttribute("class", "col fw-bold");
+            const infoAwardValue = document.createElement("div");
+            infoAwardValue.setAttribute("class", "col");
+            infoAwardValue.setAttribute("style", "width: 32vw; text-shadow: light-dark(#EAEFEF, #333446) 0px 0px 8px;");
+            
+            for (const i of infoVal.awards) {
+                const awardTitle = document.createElement("div");
+                awardTitle.setAttribute("class", "bi bi-award-fill");
+                awardTitle.innerText = i.name;
+
+                const awardValue = document.createElement("div");
+                awardValue.innerText = siding % 2 == 1 ? `— ${i.value}` : `${i.value} —`;
+
+                infoAwardTitle.append(awardTitle);
+                infoAwardValue.append(awardValue);
+            }
+
+            if (siding % 2 == 1) {
+                infoAwardCRow.append(infoAwardTitle);
+                infoAwardCRow.append(infoAwardValue);
+            } else {
+                infoAwardCRow.append(infoAwardValue);
+                infoAwardCRow.append(infoAwardTitle);
+            }
+        }
+
+        if (siding % 2 == 1) {
+            containerDiv.append(logoDiv);
+            containerDiv.append(infoDiv);
+        } else {
+            containerDiv.append(infoDiv);
+            containerDiv.append(logoDiv);
+        }
+    }
+}
+
 async function loadResumeJSON() {
     const resumeJSON = await(await fetch("/static/custom_resume.json")).json();
     appendTopInfoHTML(resumeJSON["categoryInfo"])
+    appendInfoHTML(resumeJSON["workInfo"], WORK_HTML);
+    appendInfoHTML(resumeJSON["educationInfo"], EDUCATION_HTML);
 }
 
 loadResumeJSON();
